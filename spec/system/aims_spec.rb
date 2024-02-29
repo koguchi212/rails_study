f31af8da05259e0d30a70613b8ebc6919f32e669
koguchi212 <akira.kouguchi@gmail.com>
2024-02-29 16:13:52 +0900

    詳細表示機能のspecを追加しました

--- spec/system/aims_spec.rb
+++ spec/system/aims_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 describe 'Aims', type: :system do
-  describe 'GET /aims' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+  let!(:aim_a) { FactoryBot.create(:aim, title: '目標A', user: user_a) }
 
-    before do
-      FactoryBot.create(:aim, title: '目標A', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+  describe '一覧表示機能' do
     context 'ユーザＡがログインしている場合' do
       let(:login_user) { user_a }
       it 'ユーザＡが作成したタスクが表示される' do
@@ -22,9 +21,22 @@ describe 'Aims', type: :system do
 
     context 'ユーザＢがログインしている場合' do
       let(:login_user) { user_b }
-      
+
       it 'ユーザＡが作成したタスクが表示されない' do
         expect(page).to have_no_content '目標A'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザＡがログインしている場合' do
+      let(:login_user) { user_a }
+      before do
+        visit aim_path(aim_a)
+      end
+
+      it 'ユーザＡが作成したタスクが表示される' do
+        expect(page).to have_field('aim[title]', with: '目標A')
       end
     end
   end
