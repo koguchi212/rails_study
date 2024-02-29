68c1c7d2508690cb9cdd9795c5d916b4610ec7ea
koguchi212 <akira.kouguchi@gmail.com>
2024-02-29 14:51:19 +0900

    テストでletを用いて共通化しました

--- spec/system/aims_spec.rb
+++ spec/system/aims_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 describe 'Aims', type: :system do
   describe 'GET /aims' do
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+
     before do
-      user_a = FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')
       FactoryBot.create(:aim, title: '目標A', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザＡがログインしている場合' do
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
-
+      let(:login_user) { user_a }
       it 'ユーザＡが作成したタスクが表示される' do
         expect(page).to have_content '目標A'
+      end
+    end
+
+    context 'ユーザＢがログインしている場合' do
+      let(:login_user) { user_b }
+      
+      it 'ユーザＡが作成したタスクが表示されない' do
+        expect(page).to have_no_content '目標A'
       end
     end
   end
