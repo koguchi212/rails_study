9ade067e37c3b41d5e2d2605c6b6a9d84f0cc826
koguchi212 <akira.kouguchi@gmail.com>
2024-03-08 19:20:51 +0900

    mailerのテストを書きました

--- spec/mailers/aim_mailer_spec.rb
+++ spec/mailers/aim_mailer_spec.rb
@@ -1,5 +1,36 @@
 require "rails_helper"
 
 RSpec.describe AimMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:aim) { FactoryBot.create(:aim, title: "テストmailer_spec", reason: "テスト") }
+
+  let(:text_body) do
+    part = mail.body.parts.detect { |part| part.content_type == "text/plain; charset=UTF-8" }
+    part.body.raw_source
+  end
+
+  let(:html_body) do
+    part = mail.body.parts.detect { |part| part.content_type == "text/html; charset=UTF-8" }
+    part.body.raw_source
+  end
+
+  describe "#creation_email" do
+    let(:mail) { AimMailer.creation_email(aim) }
+
+    it "想定通りのメールが生成されている" do
+      # header
+      expect(mail.subject).to eq("目標作成完了メール")
+      expect(mail.to).to eq(["user@example.com"])
+      expect(mail.from).to eq(["aim@example.com"])
+      
+      # text形式の本文
+      expect(text_body).to match("以下の目標を作成しました。")
+      expect(text_body).to match("テストmailer_spec")
+      expect(text_body).to match("テスト")  
+
+      # html形式の本文
+      expect(html_body).to match("以下の目標を作成しました。")
+      expect(html_body).to match("テストmailer_spec")
+      expect(html_body).to match("テスト")
+    end
+  end  
 end
