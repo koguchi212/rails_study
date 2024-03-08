c0a8452f3a3315c3ac1aaf5e9b511222d28f3227
koguchi212 <akira.kouguchi@gmail.com>
2024-03-08 09:47:46 +0900

    検索フォームを整えました

--- app/models/aim.rb
+++ app/models/aim.rb
@@ -6,6 +6,10 @@ class Aim < ApplicationRecord
   scope :recent, -> { order(created_at: :desc) }
 
   def self.ransackable_attributes(auth_object = nil)
-    ["advantage", "created_at", "difficulty", "id", "reason", "title", "updated_at"]
+    ["advantage", "created_at", "reason", "title", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
   end
 end
