8a4e4d9730ffdcb2c25c568d5e3b58522097073b
koguchi212 <akira.kouguchi@gmail.com>
2024-03-12 17:46:13 +0900

    seed_dataを変更しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
-Aim.all.each do |f|
-    f.destroy
-  end
-10.times do |n|
-    Aim.create!(
-      title: Faker::ProgrammingLanguage.unique.name,
-      reason: Faker::Fantasy::Tolkien.unique.character,
-      advantage: Faker::JapaneseMedia::StudioGhibli.unique.character,
-      difficulty: Faker::Number.between(from: 1, to: 10),
-    )
-  end
+User.destroy_all
+
+admin_user = User.create!(
+  name: 'admin',
+  email: 'admin@example.com',
+  admin: true,
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+Aim.destroy_all
+
+10.times do
+  admin_user.aims.create!(
+    title: Faker::ProgrammingLanguage.unique.name,
+    reason: Faker::Fantasy::Tolkien.unique.character,
+    advantage: Faker::JapaneseMedia::StudioGhibli.unique.character,
+    difficulty: Faker::Number.between(from: 1, to: 10)
+  )
+end
